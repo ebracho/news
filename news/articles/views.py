3660cfa101dd722f18553b6feb193d01d238deac
Eddie Bracho <ebracho@mail.csuchico.edu>
2016-10-18 22:31:41 -0700

    Added Foundation

--- news/articles/views.py
+++ news/articles/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def index(request):
-    if request.user.is_authenticated:
-        return HttpResponse('Hello {}!'.format(request.user.username))
-    return HttpResponse('Hello Stranger!')
+    return render(request, 'articles/index.html', {})
     
